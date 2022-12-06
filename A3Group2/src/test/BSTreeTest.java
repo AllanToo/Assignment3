6975e465e697c678d5ca149aee88556339a06f6d
Xyrille Tugade <113451914+XyrilleTugade@users.noreply.github.com>
2022-12-05 18:47:45 -0700

    Create BSTreeTest.java

---
+++ A3Group2/src/test/BSTreeTest.java
@@ -0,0 +1,67 @@
+package test;
+
+import org.junit.Test;
+
+import utilities.BSTree;
+
+public class BSTreeTest {
+	@Test
+	void getHeightTest() {
+		
+	}
+	
+	@Test
+	void getSizeTest() {
+		
+	}
+	
+	@Test
+	void checkIfEmptyTest() {
+		
+	}
+	
+	@Test
+	void clearTreeTest() {
+		
+	}
+	
+	@Test
+	void checkContainTest() {
+		
+	}
+	
+	@Test
+	void addToTreeTest() {
+		
+	}
+	
+	@Test
+	void inOrderTreeTest() {
+		
+	}
+	
+	@Test
+	void preOrderTreeTest() {
+		
+	}
+	
+	@Test
+	void postOrderTreeTest() {
+		
+	}
+	
+	@Test
+	void hasNextTest() {
+		
+	}
+	
+	@Test
+	void getRootTest() {
+		
+	}
+	
+	@Test
+	void searchTreeTest() {
+		
+	}
+}
